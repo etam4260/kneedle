7998954123c6a1103bc4559bf1e5ca2fd53ffd77
Emmet Tam <emmet_tam@yahoo.com>
2022-01-23 15:07:17 -0500

    added ability for algorithm to detect whether function is increasing or decrease. Also added the ability for algirthm to detect whether the fucntion is concave or convex. However these guesses might not be correct and so t he user can still specify what values they want in them.

--- R/kneedle.R
+++ R/kneedle.R
@@ -20,16 +20,16 @@
 #' This function allows you to find the knee of a graph
 #' @param x A vector of x coordinates.
 #' @param y A vector of y coordinates.
-#' @param decreasing Is the function increasing? Defaults to FALSE.
+#' @param decreasing Is the function increasing? Algorithm will take a guess at direction of data if not specified.
+#' @param concave Is this concave or convex? Algorithm will take a guess at concavity if not specified.
 #' @param sensitivity How sensitive should knee detection be? Defaults to 1. Can be a float/double.
-#' @param concave Is this concave or convex? Defaults to TRUE.
 #' @keywords knee
 #' @export
 #' @returns This function returns a x,y coordinate pair that corresponds to the knee point
 #' @examples 
 #' x <- kneedle(c(1,2,3,4,5), c(0,1,2,40,60))
 
-kneedle <- function(x, y, decreasing = FALSE, sensitivity = 1, concave = TRUE) {
+kneedle <- function(x, y, decreasing, concave, sensitivity = 1) {
   # Make sure inputs are correct
   if(length(x) == 0 || length(y) == 0) {
     stop("Make sure size of both inputs x and y are greater than 0")
@@ -43,12 +43,43 @@ kneedle <- function(x, y, decreasing = FALSE, sensitivity = 1, concave = TRUE) {
   }
   
   data <- matrix(unlist(list(x, y)), ncol = 2)
+  # This decreasing value has nothing to do with the user inputted value.
   data <- data[order(data[,1], decreasing = FALSE), ]
+  
+  
+  # If both decreasing and concave are not specified, then this algorithm will
+  # take a guess at those parameters instead of defaulting to certain values. 
+  # One method is to take the derivative of the starting point to the ending point
+  # from min x value to max x value. This should be similar to taking the average of all
+  # derivatives from xi to xi+1 where i = 1 to i = length(xvalues)
+  if(missing(decreasing)) {
+    # Increasing discrete data
+    if( (data[(nrow(data)), 2] - data[1, 2]) >= 0 ) {
+      decreasing = FALSE
+    # Decreasing discrete data
+    } else {
+      decreasing = TRUE
+    }
+  }
+  
+  # To determine concavity we need to look at the second derivative of the
+  # entire set of discrete data from xi to xi+1 where i = 1 to i = length(xvalues)
+  # Taking the average of all the second derivatives, if greater or equal to 0
+  # then concave up. If less than 0 then concave down. 
+  if(missing(concave)) {
+    secondderiv <- diff(diff(data[, 2]) / diff(data[, 1]))
+    if(mean(secondderiv) > 0) {
+      concave = TRUE
+    } else {
+      concave = FALSE
+    }
+  }
+  
   maxy <- max(y)
   miny <- min(y)
   maxx <- max(x)
   minx <- min(x)
-  data[ ,1] <- (data[, 1]- min(data[, 1]))/(max(data[ ,1])-min(data[, 1]))
+  data[ ,1] <- (data[, 1]- min(data[, 1]))/(max(data[ ,1])- min(data[, 1]))
   data[ ,2] <- (data[, 2]- min(data[, 2]))/(max(data[ ,2])- min(data[, 2]))
 
   if(concave && !decreasing) {
@@ -67,7 +98,6 @@ kneedle <- function(x, y, decreasing = FALSE, sensitivity = 1, concave = TRUE) {
   data <- cbind(data, differ)
 
   diffx = diff(data[, 1])
-
   T.lm.x.s <- sensitivity * mean(diffx)
   knee = NULL
   
